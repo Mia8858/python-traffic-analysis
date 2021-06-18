873331db8ea1b52a7f7615f0b2b260113cc96079
Mia8858 <qwezxc246810@gmail.com>
2021-06-18 10:51:42 +0800

    add csv

---
+++ adddata.py
@@ -0,0 +1,14 @@
+import csv
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datacenter.settings')
+django.setup()
+from mysite.models import Post
+
+with open("traffic.csv", newline="\n", encoding="utf-8") as csvfile:
+	rows = csv.reader(csvfile, delimiter=",")
+	for row in rows:
+		print(row[0], row[2])
+		newdata = Post(title=str(row[0]), body=str(row[2]))
+		newdata.save()
+	
